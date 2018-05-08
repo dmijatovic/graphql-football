64859b62b65b3b43e7e1307b3adbcb2ddf3a747c
Dusan Mijatovic <d.mijatovic@dv4all.nl>
2018-05-08 09:14:21 +0200

    Delete twitter.cfg.js

--- data/twitter.cfg.js
+++
@@ -1,8 +0,0 @@
-	
-	module.exports = {
-    	"consumerKey": "p617iLfRqEhLvFSKTQMDlMWgf",
-    	"consumerSecret": "zHTOlARoe9KjW1lfS8Jb0hZGFhcgfc86eLsr2CWNbZyOyW7HJO",
-    	"accessToken": "136677494-2Y67E2Wv7gRO46xKhv50fNGHUUbW3gkPE6ssmMeZ",
-    	"accessTokenSecret": "Q82A5zhb3iVq1olJc0qNejpaxugGOPs3pYBisSbPoBoJ2",
-    	"callBackUrl": ""
-	}
